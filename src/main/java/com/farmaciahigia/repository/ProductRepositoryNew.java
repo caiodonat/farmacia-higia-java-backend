821c3510aa1505257fba049c332bc94697546c5b
cdonat <cdonat.dev@gmail.com>
2023-07-31 03:32:47 +0000

    feat(repository): Product.selectByType

--- src/main/java/com/farmaciahigia/repository/ProductRepositoryNew.java
+++ src/main/java/com/farmaciahigia/repository/ProductRepositoryNew.java
@@ -1,31 +1,35 @@
 package com.farmaciahigia.repository;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
-// import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
-// import com.farmaciahigia.dao.ProductDao;
-// import com.farmaciahigia.model.Product;
+import com.farmaciahigia.dao.ProductDao;
+import com.farmaciahigia.model.Product;
 
-// public class ProductRepositoryNew() {
+@Service
+public class ProductRepositoryNew {
     
-// 	private String queryOne;
-//     public List<Product> myFindAll() {
-//         // TODO Auto-generated method stub
-//         return super.findAll();
-// 	}
-
-// }
-// /**
-//  * DELETE / URL/products
-//  * body:{
-//  *  products: [ 1, 2, 53, 23 ]
-//  * }
-//  * 
-//  * 
-//  * content: {
-//  *  name: "Asdas",
-//  *  urlImagem: "https://farmaciahigia.com/v1/files/uuid"
-//  * }
-//  */
+    private final ProductDao repository;
+    public ProductRepositoryNew(ProductDao repository) {
+        this.repository = repository;
+    }
+    
+    public List<Product> selectByType(String type){
+        System.out.println(type);
+        
+        return repository.findAll()
+        // .removeIf(p -> p.getType().equalsIgnoreCase(type))
+        .stream()
+        .filter(p -> (
+            p.getType().contains(type)
+        ))
+        .toList()
+        ;
+        
+    }
+    
+    
+}
